8b61e2812cb0d63b97413d9294380a9b62ace7cb
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-10-22 03:53:42 -0700

    create useSignUp hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+import Login from './pages/Login'
+import Signup from './pages/Signup';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="/login"
+            element={<Login />}
+          />
+          <Route 
+            path="/signup"
+            element={<Signup />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
