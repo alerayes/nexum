f27fea4929a070f70e3b1efb6223b156e723ee1a
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-10-22 00:02:54 -0700

    adds validation logic to static signup method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,21 @@
 const User = require('../models/userModel')
 
-// login user
+// login a user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+  res.json({mssg: 'login user'})
 }
 
-// signup user
+// signup a user
 const signupUser = async (req, res) => {
-    const {email, password} = req.body
+  const {email, password} = req.body
 
-    try {
-        const user = await User.signup(email,password)
+  try {
+    const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-
-    res.json({mssg: 'signup user'})
+    res.status(200).json({email, user})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
-module.exports = { signupUser, loginUser}
+module.exports = { signupUser, loginUser }
