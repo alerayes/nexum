9f376be3017f4c19c428f59ff1a2608e6e059621
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-10-22 01:38:27 -0700

    adds loginUser controller function and static login method

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -45,6 +45,27 @@ userSchema.statics.signup = async function (email, password) {
     return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password) {
+    
+    // validation
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
 
+    const user = await this.findOne({email})
+
+    if (!user) {
+        throw Error('Incorrect email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
