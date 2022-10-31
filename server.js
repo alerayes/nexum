fb7ef7c750e4b209d07db80cbcf2e45201be1c75
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-10-30 17:17:27 -0700

    Create basic express server and nodemon

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Server running...')
+import express from 'express'
+
+const app = express ()
+
+app.get('/', (req, res) => {
+    res.send('Welcome!')
+})
+
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
