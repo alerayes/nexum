765c6791ca47402a1679e78b49d6dc8d59ae3bc6
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-11-01 13:18:48 -0700

    Setup Error Handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,23 @@
 import User from "../models/User.js"
 import { StatusCodes } from 'http-status-codes'
+import {BadRequestError} from '../errors/index.js'
 
 
 const register = async (req, res) => {  
+    const {name, email, password} = req.body
 
-        const user = await User.create(req.body)
-        res.status(201).json({user})
+    if(!name || !email || !password){
+        throw new BadRequestError('please provide all values')
+    }
+
+    const userAlreadyExists = await User.findOne({email})
+
+    if(userAlreadyExists){
+        throw new BadRequestError('Email already in use')
+    }
+
+    const user = await User.create({name, email, password})
+    res.status(201).json({user})
 
 }
 
