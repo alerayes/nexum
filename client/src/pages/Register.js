0193bfde8aa9ddc15a2936d6f5626bcd83b407b0
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-11-02 18:06:32 -0700

    Setup Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,46 @@
+import { useState, useEffect } from 'react'
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true
+}
+
+
 const Register = () => {
+  const [values, setValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+
   return (
-    <div>
-        <h1>Register</h1>
-    </div>
+    //  Logo Here
+     <form className='register-form' onSubmit={onSubmit}>
+        <h3>Login</h3>
+        {/* Name field */}
+
+        <div className='form-row'>
+
+          <label htmlFor="name" className='form-label'>
+            Name
+          </label>
+          <input  
+            type='text'
+            value={values.name}
+            name='name'
+            onChange={handleChange}
+            className='form-input'
+          />
+
+        </div>
+     </form>
   )
 }
 
