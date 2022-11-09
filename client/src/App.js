cb5de85f91bd182af8e130352f7af0f3005edba0
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-11-08 22:06:47 -0800

    Update App.js Routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
             <Route path="/dashboard/*" element={<StudentDashboard/>}/>
             <Route path="/register" element={<Register/>}/>
             <Route path="/landing" element={<Landing/>} />
-            <Route path="*" element={<Error/>}/>
+            {/* <Route path="*" element={<Error/>}/> */}
           </Routes>
       </BrowserRouter>      
     </div>
