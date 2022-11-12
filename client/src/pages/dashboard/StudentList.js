5ae4625cb898c144c6893a6ac2cef924b43ddc34
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2022-11-11 16:12:57 -0800

    Create StudentList

--- client/src/pages/dashboard/StudentList.js
+++ client/src/pages/dashboard/StudentList.js
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react"
+
 const StudentList = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = await fetchUsers()
+      setUsers(users)
+    }
+    getUsers()
+  }, [users])
+
+  // Fetch Users
+  const fetchUsers = async () => {
+    const res = await fetch('/api/v1/auth/getUsers')
+    const data = await res.json()
+
+    console.log(data)
+    return data
+  }
+
+   
+
   return (
-    <h1>Student List</h1>
+    <div>
+      {
+        users.length && users.map((user) => (
+          <div key={user._id}>
+            <p>{user.name}</p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
