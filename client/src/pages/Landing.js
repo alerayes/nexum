4b89900ea3d04f58183b0b5c3e511759a1061b48
FieldPro <61987859+FieldPro@users.noreply.github.com>
2022-11-12 01:04:14 +0000

    Update Landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,195 +2,37 @@
 import { Link } from 'react-router-dom'
 import LogoIcon from '../assets/nexum_logo.png'
 import Footer from '../component/footer'
+import Hero from '../components/Hero'
+import Infosession1 from '../components/Infosession1'
+import Infosession2 from '../components/Infosession2'
+import Infosession3 from '../components/Infosession3'
+import Footerland from '../components/Footerland'
 
 const Landing = () => {
-     return (
+    return (
     <div> 
-        <header className="Landing">
-            <div className="logo">
-
+        <div className="landingHeader">
+            <div className="navLogo">
+                <img className="landingImg" src={LogoIcon} />
             </div>
 
-            <div className="Nav">
-                <Link to="/"> Home </Link>
-                <Link to="/"> Features </Link>
-                <Link to="/"> Our Team </Link>
-                <Link to="/"> Contact </Link>
-                <Link to="/register"><button>Login</button></Link>
+            <div className="navList">
+                <div><Link to="/"> Home </Link></div>
+                <div><Link to="/"> Features </Link></div>
+                <div><Link to="/"> Our Team </Link></div>
+                <div><Link to="/"> Contact </Link></div>
+                <div><Link to="/register"><button className="lgBtn">Login</button></Link></div>
             </div>
-        </header>
-        
-        {/* Hero */}
+        </div>
 
-        {/* <section className="hero">
-            <div className="content">
-                <h1 className="hero_title">
-                
-                </h1>
-                    
-                <h2 className="subtitle">
-                
-                </h2>
-            </div>
-        </section>
+        <Hero />
+        <Infosession1 />
+        <Infosession2 />
+        <Infosession3 />
 
-       
-        <h2> Main Features </h2> 
-        <div className="columns">
-            <div className="column">
-                <h3>Feature 1</h3>
-            </div>
-                <img src="" alt="Feature 1" />
-            <div className="column">
-                
-            </div>
-
-            <div className="column">
-                <img src="" alt="Feature 2" />
-            </div>
-
-            <div className="column">
-                <h3>Feature 2</h3>
-            </div>
-
-            <div className="column">
-                <h3>Feature 3</h3>
-            </div>
-
-            <div className="column">
-                <img src="" alt="Feature 3" />
-            </div>
-        </div> */}
-        
-        {/* <div>
-            <div>
-                <div>
-                    <h3>Increase your Network in your University</h3>
-                </div>
-                <div>
-                    <Link to="/login"><button>Login</button></Link>
-                </div>
-            </div>
-            
-           
-
-            <div>
-                <div className="column">
-                    <img src="" alt="" />
-                    <div className="devDetails">
-                        <h3>Name</h3>
-                        <h3>Position</h3>
-                        <h3>LinkedIn</h3>
-                    </div>
-                </div>
-
-                <div className="column">
-                    <img src="" alt="" />
-                    <div className="devDetails">
-                        <h3>Name</h3>
-                        <h3>Position</h3>
-                        <h3>LinkedIn</h3>
-                    </div>
-                </div>
-
-                <div className="column">
-                    <img src="" alt="" />
-                    <div className="devDetails">
-                        <h3>Name</h3>
-                        <h3>Position</h3>
-                        <h3>LinkedIn</h3>
-                    </div>
-                </div>
-            </div>
-
-            <div>
-                <div className="column">
-                    <img src="" alt="" />
-                    <div className="devDetails">
-                        <h3>Name</h3>
-                        <h3>Position</h3>
-                        <h3>LinkedIn</h3>
-                    </div>
-                </div>
-
-                <div className="column">
-                    <img src="" alt="" />
-                    <div className="devDetails">
-                        <h3>Name</h3>
-                        <h3>Position</h3>
-                        <h3>LinkedIn</h3>
-                    </div>
-                </div>
-
-                <div className="column">
-                    <img src="" alt="" />
-                    <div className="devDetails">
-                        <h3>Name</h3>
-                        <h3>Position</h3>
-                        <h3>LinkedIn</h3>
-                    </div>
-                </div>
-
-                <div className="column">
-                    <img src="" alt="" />
-                    <div className="devDetails">
-                        <h3>Name</h3>
-                        <h3>Position</h3>
-                        <h3>LinkedIn</h3>
-                    </div>
-                </div>
-            </div>
-            
-        </div> */}
-
-        {/* <div className="footer">
-            <div className="landingFooter">
-
-                <div className="footerAbout">
-                    <h2>Contact Nexum</h2>
-                    <p>We're here to help and answer any questions you might
-                        have. We look forward to hearing from you.
-                    </p>
-                </div>
-
-                <div className="whiteBg">
-                    <form>
-                        <fieldset>
-                            <legend><label for="fname">First name*</label>
-                        </fieldset>
-
-                        <fieldset>
-                            <legend><label for="fname">Last name*</label>
-                        </fieldset>
-
-                        <fieldset>
-                            <legend><label for="fname">Email</label>
-                        </fieldset>
-
-                        <fieldset>
-                            <legend><label for="fname">Message</label>
-                        </fieldset>
-
-                        <!-- <textarea rows="4" cols="50" name="comment" form="usrform">
-                        </textarea> -->
-
-                        <input type="submit" value="Submit" />
-                    </form>
-                </div>
-            </div>
-            <hr>
-            <div>
-                <div>
-                    <img className='img-logo' src={LogoIcon} alt="logo" />
-                </div>
-                <div>
-                    <p>Nexum 2022</p>
-                </div>
-            </div>
-        </div> */}
-    </div>
-
-    )  
+        <Footerland />
+    </div>    
+    )   
 }
  
 export default Landing;
