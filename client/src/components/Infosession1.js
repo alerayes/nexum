6a70bde2d40e13e47bc1d84a399465289b0f5371
FieldPro <61987859+FieldPro@users.noreply.github.com>
2022-11-11 17:10:02 -0800

    Commit changes

---
+++ client/src/components/Infosession1.js
@@ -0,0 +1,54 @@
+const Infosession1 = () => {
+    return (   <>
+        <div className="column">
+            <h1 className="columMain"> Main Features </h1>
+            
+            <div className="forMain">
+                <div className="columnFt1">
+                    <h2>Feature 1</h2>
+                    <div>
+                        Brief description about feature 1. Lorem ipsum dolor sit amet<br /> 
+                        consectetur adipisicing elit. Non corporis, omnis natus,<br /> 
+                        excepturi iusto iste aliquid obcaecati nemo ab eveniet ut<br /> 
+                        rerum porro sit molestiae cum fugiat? Iste, ratione aspernatur?.
+                    </div>
+                </div>
+
+                <div className="columnFt1Photo">
+                    <img src="" alt="Feature 1" />
+                </div>
+
+                <div className="columnFt2Photo">
+                    <img src="" alt="Feature 2" />
+                </div>
+                
+                <div className="columnFt2">
+                    <h2>Feature 2</h2>
+                    <div>
+                        Brief description about feature 1. Lorem ipsum dolor sit amet<br /> 
+                        consectetur adipisicing elit. Non corporis, omnis natus,<br /> 
+                        excepturi iusto iste aliquid obcaecati nemo ab eveniet ut<br /> 
+                        rerum porro sit molestiae cum fugiat? Iste, ratione aspernatur?.
+                    </div>
+                </div>
+
+                <div className="columnFt3">
+                    <h2>Feature 3</h2>
+                    <div>
+                        Brief description about feature 1. Lorem ipsum dolor sit amet<br /> 
+                        consectetur adipisicing elit. Non corporis, omnis natus,<br /> 
+                        excepturi iusto iste aliquid obcaecati nemo ab eveniet ut<br /> 
+                        rerum porro sit molestiae cum fugiat? Iste, ratione aspernatur?.
+                    </div>
+                </div>
+
+                <div className="columnFt3Photo">
+                    <img src="" alt="Feature 3" />
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+ 
+export default Infosession1;
---
+++ client/src/components/Infosession3.js
@@ -0,0 +1,45 @@
+const Infosession3 = () => {
+    return (   <>
+        <div className="columnDev2">
+            <div>
+                <img src="" alt="image" />
+                <div className="devDetails">
+                    <h3>Name</h3>
+                    <p>Position</p>
+                    <p>LinkedIn</p>
+                </div>
+            </div>
+
+            <div>
+                <img src="" alt="image" />
+                <div className="devDetails">
+                    <h3>Name</h3>
+                    <p>Position</p>
+                    <p>LinkedIn</p>
+                </div>
+            </div>
+
+            <div>
+                <img src="" alt="image" />
+                <div className="devDetails">
+                    <h3>Name</h3>
+                    <p>Position</p>
+                    <p>LinkedIn</p>
+                </div>
+            </div>
+
+            <div>
+                <img src="" alt="image" />
+                <div className="devDetails">
+                    <h3>Name</h3>
+                    <p>Position</p>
+                    <p>LinkedIn</p>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+ 
+export default Infosession3;
+
