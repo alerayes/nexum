ca331a5b24e7bb4a54f1f746c4860c2559670a37
FieldPro <61987859+FieldPro@users.noreply.github.com>
2022-11-11 16:58:36 -0800

    Hero section

---
+++ Hero.js
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return (   <>
+        <div className="hero">
+            <div className="heroD">
+                <h1>About Nexum</h1>
+                <p>Brief description about the platform.</p>
+                <button className="chooseBtn">Choose Your Plan</button>
+            </div>
+        </div>
+        </>
+    );
+}
+ 
+export default Hero;
+
