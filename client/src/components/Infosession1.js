7cf75471a55fde9cca4d0429de927b95f7516cab
bhanuj <nagpal.bhanuj@gmail.com>
2022-11-30 12:32:29 -0800

    added layout for landing page

--- client/src/components/Infosession1.js
+++ client/src/components/Infosession1.js
@@ -11,11 +11,7 @@ const Infosession1 = () => {
                     <h2>Manage Students and Alumni</h2>
                     <div>
                         <p>
-                        Our SaaS application allows institution administrators to <br/>
-                         track and manage students and alumni.
-                        Admins are provided an <br/> intuitive way to access information in list and graphical formats. <br/>The UI
-                        is focused on providing simplicity and functional
-                        behavior.
+                        Our SaaS application allows institution administrators to track and manage students and alumni.Admins are provided an intuitive way to access information in list and graphical formats.The UI is focused on providing simplicity and functional behavior.
                         </p>
                         {/* Brief description about feature 1. Lorem ipsum dolor sit amet<br /> 
                         consectetur adipisicing elit. Non corporis, omnis natus,<br /> 
@@ -37,12 +33,7 @@ const Infosession1 = () => {
                 <div className="columnFt2">
                     <h2>Networking</h2>
                         <p>
-                        Enhancing communication between students <br/>
-                        and alumni is the core of our application. <br/>
-                        Everything we have built revolves around improving the<br/> interaction of students with other 
-                        students, alumni and job <br/>opportunities that 
-                        are shared within a private community of the <br/>
-                        institution.
+                        Enhancing communication between students and alumni is the core of our application. Everything we have built revolves around improving the interaction of students with other students, alumni and job opportunities that are shared within a private community of the institution.
                         </p>
                     {/* <div>
                         Brief description about feature 1. Lorem ipsum dolor sit amet<br /> 
@@ -56,13 +47,7 @@ const Infosession1 = () => {
                 <div className="columnFt3">
                     <h2>Find and Share jobs</h2>
                     <p>
-                    Our application tries to make it easier for
-                    students <br/>by sharing  exclusive community job
-                    oppportunities that <br/>are visible to the students
-                    and alumnis of the instituition. These <br/> job
-                    listings make it easier and more comfortable
-                    for students <br/>as they try to fnd a job in their
-                    industry.
+                    Our application tries to make it easier for students by sharing  exclusive community job oppportunities that are visible to the students and alumnis of the instituition. These  job listings make it easier and more comfortable for students as they try to fnd a job in their industry.
                     </p>
                     {/* <div>
                         Brief description about feature 1. Lorem ipsum dolor sit amet<br /> 
