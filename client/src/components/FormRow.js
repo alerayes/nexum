21e39e9294272dfc9c7ab1f44e030ea9f21cafa9
FieldPro <61987859+FieldPro@users.noreply.github.com>
2022-12-01 18:23:24 +0000

    Form update

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -11,6 +11,7 @@ const FormRow = ({type, name, value, handleChange, labelText}) => {
             onChange={handleChange}
             className='form-input'
         />
+              
     </div>
   )
 }
